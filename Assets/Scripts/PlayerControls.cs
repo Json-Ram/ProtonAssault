59543d12d19270375cd2ab382510b334ef193cc5
Json-Ram <jasonram778@gmail.com>
2023-05-13 19:32:27 -0700

    Create PlayerControls script, basic logic

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerControls : MonoBehaviour
+{
+    [SerializeField] InputAction movement;
+
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        movement.Enable();    
+    }
+
+    void OnDisable()
+    {
+        movement.Disable();    
+    }
+
+    void Update()
+    {
+        float xThrow = movement.ReadValue<Vector2>().x;  
+        float yThrow = movement.ReadValue<Vector2>().y;
+
+        //Debug.Log(xThrow);
+        //Debug.Log(yThrow);    
+    }
+}
