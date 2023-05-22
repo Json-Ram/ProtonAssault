0efb3c9b3a762115b7958d0237b799e7205870a5
Json-Ram <jasonram778@gmail.com>
2023-05-21 18:22:47 -0700

    Create ScoreBoard script, enemies now have a score when killed

---
+++ Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBoard : MonoBehaviour
+{
+    int score;
+    public void IncreaseScore(int increaseAmount)
+    {
+        score += increaseAmount;
+        Debug.Log($"Your score is: {score}");
+    }
+}
